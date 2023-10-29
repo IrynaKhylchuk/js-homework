5e0a18b61747a9fd479d42f09f54556a77cc0f22
Iryna Khylchuk <khylchuk@icloud.com>
2023-10-29 14:16:53 +0200

    finish task 2
    minor updates to logs in task 1

--- homework-8/script.js
+++ homework-8/script.js
@@ -9,14 +9,14 @@ console.log(arrayNumbers)
 arrayNumbers.sort((x, y) => x - y)
 console.log(arrayNumbers)
 
-const sum = arrayNumbers.reduce((total, amount) =>{
+let sum = arrayNumbers.reduce((total, amount) =>{
     if (amount < 0) {
         return total + amount
     }
     return total
 })
 
-console.log(sum)
+console.log(`Task 1: Sum = ${sum}`)
 
 //1
 let product = 1
@@ -24,7 +24,7 @@ for (let i = 1; i < arrayNumbers.length - 1; i++) {
     const number = arrayNumbers[i]
     product *= number
 }
-console.log(product)
+console.log(`Task 1: Product v1 = ${product}`)
 
 //2
 product = 1
@@ -34,5 +34,37 @@ arrayNumbers.forEach((number, index) => {
         product *= number
     }
 })
-console.log(product)
+console.log(`Task 1: Product v2 = ${product}`)
 
+// Завдання 2:
+// Задано масив з числовими значеннями 45, -65, 48, 52, -45, -78, -96, 25, 14, 25, 45;
+// В масиві визначити добуток елементів з парними номерами.
+// Знайти суму елементів, розташованих між найбільшим та найменшим елементами.
+
+const arrayNumbers2 = [45, -65, 48, 52, -45, -78, -96, 25, 14, 25, 45]
+
+console.log(arrayNumbers2)
+arrayNumbers2.sort((x, y) => x - y)
+console.log(arrayNumbers2)
+
+sum = 0
+
+arrayNumbers2.forEach((number, index) => {
+    if (index > 0 && index < arrayNumbers2.length - 1)
+    {
+        sum += number
+    }
+})
+
+console.log(`Task 2: Sum between min and max = ${sum}`)
+
+let reduce = 1
+
+arrayNumbers2.forEach((number) => {
+    if (number % 2 === 0)
+    {
+        reduce *= number
+    }
+})
+
+console.log(`Task 2: Product of even numbers = ${reduce}`)
